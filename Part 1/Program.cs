e0b0df63c5fd3a2ac06f47a0cdabb26d97d6903b
Antonio <antonioareas@me.com>
2020-10-04 21:29:54 -0700

    Add project files.

---
+++ Part 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/// Week 5		Homework 1
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  October 4th 2020
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declare arrays
+            int[] original = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] copy = new int[original.Length];
+
+            CopyArray(original, copy);
+
+            //print both arrays
+            Console.WriteLine("Original:");
+            PrintArray(original);
+
+            Console.WriteLine("Copy:");
+            PrintArray(copy);
+
+        }
+
+        //function that copies two arrays of the same length
+        static void CopyArray(int[] original, int[] copy)
+        {
+
+            if (original.Length != copy.Length)
+            {
+                Console.WriteLine("These two arrays have different lengths");
+                return;
+            }
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                copy[i] = original[i];
+            }
+
+        }
+
+        //printarray function
+        static void PrintArray(int[] array)
+        {
+
+            foreach (int element in array)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+
+    }
+}
---
+++ Part 2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+/// Week 5		Homework 2
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  October 4th 2020
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = GenerateArray(10);
+            numbers = Reverse(numbers);
+            PrintArray(numbers);
+
+        }
+
+        //generate array of int type
+        static int[] GenerateArray(int length)
+        {
+            //generate array of given length
+            int[] tempArray = new int[length];
+
+            Console.WriteLine("Enter {0} numbers.", length);
+            
+            //generate array based on given length
+            for (int i = 0; i < length; i++)
+            {
+                tempArray[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            return tempArray;
+
+        }
+
+        //reverse array of int type
+        static int[] Reverse(int[] array)
+        {
+            //temp holding variables
+            int temp, a, b;
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                //set to temp variables, accounting for 0 index
+                a = array[i];
+                b = array[array.Length - i - 1];
+
+                //make the swap
+                temp = a;
+                a = b;
+                b = temp;
+
+                //set indexes to corresponding numbers, accounting for 0 index
+                array[i] = a;
+                array[array.Length - i - 1] = b;
+
+            }
+            
+            return array;
+
+        }
+
+        //print array of int type
+        static void PrintArray(int[] array)
+        {
+
+            foreach (int element in array)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+
+    }
+}
